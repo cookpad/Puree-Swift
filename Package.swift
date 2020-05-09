3e1c344e9790ce8866a5ccf577f94db20cbfc891
aomathwift <a.okw1011@gmail.com>
2020-05-09 15:39:44 +0900

    Fix support platform

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Puree",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_12),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
